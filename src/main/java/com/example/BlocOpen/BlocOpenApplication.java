598f3423cc68567188f5a94f1f552c74d7e27ccf
Javier-Cs <krchipullajavier@gmail.com>
2025-05-31 14:37:03 -0500

    un inicio bueno en el blog

---
+++ src/main/java/com/example/BlocOpen/BlocOpenApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlocOpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlocOpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlocOpenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BlocOpen/BlocOpenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BlocOpen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlocOpenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
